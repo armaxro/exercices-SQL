9891f6ae36f1a35b94d30bd4cd07ae82c870754f
Maxi Rojas <arnaldomrojas@outlook.com>
2024-05-21 22:13:09 +0200

    update

--- maxi-exe1.sql
+++ maxi-exe1.sql
@@ -1,10 +1,17 @@
 -- Sélectionnez tous les champs de `categ` ordonnés par `name` ascendant
--- Séléctionnez `idcateg` et `name` de `categ` quand `idcateg` vaut 4
+SELECT * FROM categ ORDER BY name ASC; 
+-- Séléctionnez `idcateg` et `name` de `categ` quand `idcateg` vaut 4+
+SELECT idcateg, name FROM categ WHERE idcateg =4; 
 -- Séléctionnez `idcateg` et `name` de `categ` quand `idcateg` se trouve entre 2 et 4
+SELECT idcateg, name FROM categ WHERE idcateg BETWEEN 2 AND 4; 
 -- Séléctionnez `idcateg` et `name` de `categ` quand `idcateg` est 1, 3 ou 5  ordonné par `name` descendant
+SELECT idcateg, name FROM categ WHERE idcateg IN (1, 3, 5) ORDER BY name DESC; 
 -- Séléctionnez tous les champs de `categ` quand `desc` contient 'et' n'importe où dans la chaîne
+SELECT * FROM categ WHERE `desc` LIKE '%et%'; 
 -- Séléctionnez tous les champs de `categ` dont l' `idcateg` vaut 5 ainsi que les `idnews` et  `title` de la table `news` qui se trouvent dans cette catégorie, même si il n'y en a pas (présence de `categ` dans tous les cas, 17 lignes de résultats) , ordonnés par `news`.`title` ASC
+
 -- Séléctionnez tous les champs de `categ` dont l' `idcateg` vaut 5 ainsi que les `idnews` et  `title` de la table `news` qui se trouvent dans cette catégorie, même si il n'y en a pas (présence de `categ` dans tous les cas, 6 lignes de résultats) , ordonnés par `news`.`title` ASC ET que `news`.`visible` vaut 1 !
+SELECT * FROM categ WHERE idcateg = 5
 -- Séléctionnez tous les champs de `categ` dont l' `idcateg` vaut 5 ainsi que les `idnews` (concaténés sur une seul ligne avec la ',' comme séparateur) et  `title` (concaténés sur une seul ligne avec '|||' comme séparateur) de la table `news` qui se trouvent dans cette catégorie, même si il n'y en a pas (présence de `categ` dans tous les cas, 1 ligne de résultats) ,  ET que `news`.`visible` vaut 1 !
 -- Séléctionnez `idnews` et `title` de la table `news` lorsque le `title` commence par 'c' (7 résultats)
 -- Séléctionnez `idnews` et `title` de la table `news` lorsque le `title` commence par 'a' et `visible` vaut 1 (10 résultats)
